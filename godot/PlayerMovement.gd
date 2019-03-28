4d86f68fd722e96bdf7ef33c8074d18ce050bc20
J.A. Shue <j.a.shue@outlook.com>
2019-03-28 07:53:08 -0800

    Working player movements! Still need to do taxicab motion.

---
+++ godot/PlayerMovement.gd
@@ -0,0 +1,21 @@
+extends KinematicBody2D
+
+export (int) var speed = 200
+
+var velocity = Vector2()
+
+func get_input():
+    velocity = Vector2()
+    if Input.is_action_pressed('right'):
+        velocity.x += 1
+    elif Input.is_action_pressed('left'):
+        velocity.x -= 1
+    elif Input.is_action_pressed('down'):
+        velocity.y += 1
+    elif Input.is_action_pressed('up'):
+        velocity.y -= 1
+    velocity = velocity.normalized() * speed
+
+func _physics_process(delta):
+    get_input()
+    move_and_slide(velocity)
